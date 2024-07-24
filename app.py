39b3bef787674d58e24078617a56ab799ae069c9
dederico <dederico@gmail.com>
2024-07-24 16:04:33 -0600

    port error handling

--- app.py
+++ app.py
@@ -118,4 +118,5 @@ def message():
     return str(message.sid)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
